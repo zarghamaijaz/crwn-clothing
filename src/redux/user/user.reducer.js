6b9e49525075bf4aa5ab0cfc60691e0b802b30d5
zarghamaijaz <zarghamaijaz45@gmail.com>
2022-10-22 23:32:26 +0500

    Redirect to home implemented

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,9 +1,10 @@
+import { UserActionTypes } from "./user.types";
 const initialState = {
     currentUser:null
 }
 const userReducer = (state = initialState, action) => {
     switch (action.type){
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser:action.payload
