bb73bc233e0e75207e37c7149bb09d44e7283e15
zarghamaijaz <zarghamaijaz45@gmail.com>
2022-10-23 20:50:35 +0500

    added cart icon, made cart icon toggleable with redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) =>{
+const Header = ({ currentUser, hidden }) =>{
     return(
         <div className="header">
             <Link className="logo-container" to='/'>
@@ -21,11 +23,14 @@ const Header = ({ currentUser }) =>{
                     :
                     <Link className="option" to="/signin">SIGN IN</Link>
                 }
+                <CartIcon/>
             </div>
+            {hidden ? null : <CartDropdown/>}
         </div>
     )
 }
-const mapStateToProps = (state) =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ( { user:{currentUser}, cart:{ hidden } } ) =>({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
