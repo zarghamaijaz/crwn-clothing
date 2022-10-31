0f547310165bd0afbc8ce612fa3278bd5d567604
zarghamaijaz <zarghamaijaz45@gmail.com>
2022-10-31 23:26:40 +0500

    Added memorized selectors to our app

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
 
 import CustomButton from "../custom-button/custom-button.component";
@@ -20,8 +21,8 @@ const CartDropdown = ({ cartItems }) => {
     )
 }
 
-const mapStateToProps = ({ cart:{ cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems:selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
